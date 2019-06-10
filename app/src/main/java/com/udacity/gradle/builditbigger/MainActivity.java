cfb87ccd9c418c670d6ef05473f6d3d737a47f20
Jason Arendt <jasonarendt1@gmail.com>
2019-06-10 15:27:06 -0500

    Udacity Build it bigger joke app

---
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -0,0 +1,65 @@
+package com.udacity.gradle.builditbigger;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.android.jokedisplayactivity.DisplayJokeActivity;
+import com.example.android.jokelib.JokeCreator;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+
+public class MainActivity extends AppCompatActivity {
+    private AdView mAdView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(this, "@string/ad_id_main");
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void tellJoke(View view) {
+        JokeCreator jokeCreator = new JokeCreator();
+
+        Intent intent = new Intent(this, DisplayJokeActivity.class);
+        intent.putExtra(getString(R.string.jokeEnvelope), jokeCreator.getJokes());
+        startActivity(intent);
+    }
+
+
+}
---
+++ jokelib/src/main/java/com/example/android/jokelib/JokeCreator.java
@@ -0,0 +1,29 @@
+package com.example.android.jokelib;
+
+public class JokeCreator {
+
+    public String getJokes() {
+        return "why so serious?!?!";
+    }
+
+
+//    private String[] jokes;
+//    private Random random;
+//    //TODO ADD IN JOKES
+//    public JokeCreator() {
+//        jokes = new String[3];
+//        jokes[0] = "Joke1";
+//        jokes[1] = "Joke2";
+//        jokes[2] = "Joke3";
+//        random = new Random();
+//    }
+//
+//    public String[] getJokes() {
+//        return jokes;
+//    }
+//
+//    public String getRandomJoke() {
+//        return jokes[random.nextInt(jokes.length)];
+//    }
+
+}
